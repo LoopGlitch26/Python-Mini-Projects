fd7802b380fcf99618a5210f6152a882dafcf23a
Bravish Ghosh <grandson.baba.2012@gmail.com>
2020-05-14 23:15:54 +0530

    Added tkinter for GUI

--- ScreenShotPy.py
+++ ScreenShotPy.py
@@ -1,4 +1,14 @@
 import pyautogui
+from tkinter import*
 
-mySS = pyautogui.screenshot()
-mySS.save(r'E:\PyCharm\mySSpy.jpg')  #r'Path to save the screenshot\file name.png or jpg'
+win = Tk()
+win.title("LoopGlitch Screenshoter")
+
+def callback():
+    mySS = pyautogui.screenshot()
+    mySS.save(r'E:\PyCharm\mySSpy.jpg')  #r'Path to save the screenshot\file name.png or jpg'
+
+button = Button(win, text = "Screenshot This !", command = callback)
+button.grid(row = 50, column = 50)
+
+win.mainloop()
